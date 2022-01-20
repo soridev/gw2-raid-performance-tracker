d85bbdaaa377f077e580020d1eb84fd1608aae3a
Danni <daniel.ostermann1997@gmail.com>
2022-01-20 19:51:46 +0100

    formatting

--- arc_kafka_consumer.py
+++ arc_kafka_consumer.py
@@ -6,14 +6,16 @@ from json import loads
 
 from config_helper import ConfigHelper
 
-def consume_messages(bt_servers, kafka_topic):
-    k_consumer = KafkaConsumer(kafka_topic,
-                               bootstrap_servers=bt_servers,
-                               auto_offset_reset='earliest',
-                               enable_auto_commit=True,
-                               consumer_timeout_ms=1000,
-                               value_deserializer=lambda x: loads(x.decode('utf-8')))
 
+def consume_messages(bt_servers, kafka_topic):
+    k_consumer = KafkaConsumer(
+        kafka_topic,
+        bootstrap_servers=bt_servers,
+        auto_offset_reset="earliest",
+        enable_auto_commit=True,
+        consumer_timeout_ms=1000,
+        value_deserializer=lambda x: loads(x.decode("utf-8")),
+    )
 
     print("we are waiting for messages")
 
@@ -21,16 +23,18 @@ def consume_messages(bt_servers, kafka_topic):
         info = message.value
         print(str(info))
 
+
 def main():
-    base_path = os.path.dirname(__file__)    
+    base_path = os.path.dirname(__file__)
     config = configparser.ConfigParser()
-    config.read(os.path.join(base_path, 'config.ini'))            
+    config.read(os.path.join(base_path, "config.ini"))
 
     # load settings for kafka infrastructure from settings file
-    kafka_bootstrap_servers = loads(config['kafka']['BootstrapServers'])    
+    kafka_bootstrap_servers = loads(config["kafka"]["BootstrapServers"])
     arc_topic = config.get("kafka", "ArcTopic")
 
     consume_messages(kafka_bootstrap_servers, arc_topic)
+
 
 if __name__ == "__main__":
     main()
--- arc_kafka_producer.py
+++ arc_kafka_producer.py
@@ -15,7 +15,7 @@ from arc_log_generator import generate_raw_data
 
 
 class ArcWatchDog:
-    def __init__(self, logpath:str) -> None:
+    def __init__(self, logpath: str) -> None:
         self.observer = Observer()
         self.logpath = logpath
 
@@ -36,14 +36,14 @@ class ArcWatchDog:
 
         self.observer.join()
 
+
 class Handler(FileSystemEventHandler):
-    
     @staticmethod
     def on_any_event(event):
         if event.is_directory:
             return None
-        
-        elif event.event_type == 'created':
+
+        elif event.event_type == "created":
             # Event is created, you can process it now
             print("Found newly created file:  % s." % event.src_path)
 
@@ -51,33 +51,43 @@ class Handler(FileSystemEventHandler):
             generate_raw_data(event.src_path, ei_settings_file, base_path)
             input_file_name = str.split(os.path.basename(event.src_path), ".")[0]
 
-            json_result = find_file_by_name(input_file_name, os.path.join(base_path, "resources"))
+            json_result = find_file_by_name(
+                input_file_name, os.path.join(base_path, "resources")
+            )
             json_result_file = None
 
             if len(json_result) == 1:
                 json_result_file = json_result[0]
             else:
-                raise Exception("Seems like we have duplicate .json files in the /resources folder.")
-            
+                raise Exception(
+                    "Seems like we have duplicate .json files in the /resources folder."
+                )
+
             # push info into kafka stream
-            kafka_message = {'input-file': event.src_path, 'ei-json-file': json_result_file}
+            kafka_message = {
+                "input-file": event.src_path,
+                "ei-json-file": json_result_file,
+            }
             produce_message(kafka_bootstrap_servers, arc_topic, kafka_message)
 
             print("successfuly pushed message into kafka stream.")
-        
-        elif event.event_type == 'modified':
+
+        elif event.event_type == "modified":
             # Event is modified, you can process it now
             pass
 
 
 def produce_message(bootstrap_servers, topic_name, message):
-    k_producer = KafkaProducer(bootstrap_servers=bootstrap_servers, value_serializer=lambda x: dumps(x).encode('utf-8'))
+    k_producer = KafkaProducer(
+        bootstrap_servers=bootstrap_servers,
+        value_serializer=lambda x: dumps(x).encode("utf-8"),
+    )
     k_producer.send(topic_name, value=message)
 
 
 def find_file_by_name(name, path):
-    result = []    
-    for root, dirs, files in os.walk(path):        
+    result = []
+    for root, dirs, files in os.walk(path):
         for item in files:
             if name in item:
                 result.append(os.path.join(root, item))
@@ -87,26 +97,28 @@ def find_file_by_name(name, path):
 
 def main():
     global base_path
-    base_path = os.path.dirname(__file__)    
+    base_path = os.path.dirname(__file__)
 
     config = configparser.ConfigParser()
-    config.read(os.path.join(base_path, 'config.ini'))            
-    
+    config.read(os.path.join(base_path, "config.ini"))
+
     # load kafka config from file
     global ei_settings_file
     global arc_base_dir
     global kafka_bootstrap_servers
     global arc_topic
-    
-    kafka_bootstrap_servers = loads(config['kafka']['BootstrapServers'])    
-    arc_topic = config.get("kafka", "ArcTopic")
-    ei_settings_file = os.path.join(base_path, config.get("elite-insights", "ei_config_file"))
-    arc_base_dir = config.get("elite-insights", "logfolder")
 
+    kafka_bootstrap_servers = loads(config["kafka"]["BootstrapServers"])
+    arc_topic = config.get("kafka", "ArcTopic")
+    ei_settings_file = os.path.join(
+        base_path, config.get("elite-insights", "ei_config_file")
+    )
+    arc_base_dir = config.get("elite-insights", "logfolder")
 
     # start filesystem watcher
     wd = ArcWatchDog(arc_base_dir)
     wd.run()
+
 
 if __name__ == "__main__":
     main()

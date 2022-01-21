d4c70953121c5cef9055b01bdbe3e2be1f75a8f6
Danni <daniel.ostermann1997@gmail.com>
2022-01-21 23:32:27 +0100

    let consumer produce database entries

--- arc_kafka_consumer.py
+++ arc_kafka_consumer.py
@@ -1,24 +1,38 @@
+from os import path
 from kafka import KafkaConsumer
 from json import loads
 
 from config_helper import ConfigHelper
+from arc_data_transformator import ArcDataTransformator
 
 
 def consume_messages(bt_servers, kafka_topic):
     k_consumer = KafkaConsumer(
         kafka_topic,
         bootstrap_servers=bt_servers,
-        auto_offset_reset="earliest",
+        auto_offset_reset="latest",
         enable_auto_commit=False,
-        consumer_timeout_ms=1000,
+        # consumer_timeout_ms=1000,
         value_deserializer=lambda x: loads(x.decode("utf-8")),
     )
 
     print("Starting kafka consumer - waiting for new messages.")
 
+    new_log_ids = []
+    ark_transformator = ArcDataTransformator()
+
     for message in k_consumer:
-        info = message.value
-        print(str(info))
+        message_content = message.value
+        print(message_content["id"])
+
+        log_id = ark_transformator.register_arclog_into_db(
+            evtc_name=message_content["input-file"],
+            path_to_json_file=message_content["ei-json-file"],
+        )
+
+        new_log_ids.append(log_id)
+
+    print(f"{len(new_log_ids)} new logs added to database.")
 
 
 def main():

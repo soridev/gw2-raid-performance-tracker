5f3ec31f2a63cfbda7ced69aed498ef95a1a2ad2
Danni <daniel.ostermann1997@gmail.com>
2022-10-24 22:06:02 +0200

    polish login page

--- EliteInsider/EliteInsider/settings.py
+++ EliteInsider/EliteInsider/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+LOGIN_REDIRECT_URL = "elite-ui/dashboard/"
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",

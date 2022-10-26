4692a7b5844b3da8660bcd3c864191ede9c2e3d7
Danni <daniel.ostermann1997@gmail.com>
2022-10-26 22:46:59 +0200

    start work on dashboard

--- EliteInsider/elite_insider_ui/static/js/dashboard.js
+++ EliteInsider/elite_insider_ui/static/js/dashboard.js
@@ -1,28 +1,74 @@
-var data;
+var defaultColorLayout = {
+    textColor : "#F7F6FB",
+    borderColor : "#F7F6FB",
+}
 
 function initGraph() {
+    function initColorLayout() {
+        Chart.defaults.color = defaultColorLayout.textColor;
+        Chart.defaults.borderColor = defaultColorLayout.borderColor;
+    }
+
     function initBarChart() {
         axios.get(restAPIUrl + "fullclear-stats/ZETA/").then(function (response) {
             let data = response.data;
-            let labels = [];
-            let ktimes = [];
+            let labels = []
+            let ktTime = []
 
             for (let i = 0; i < data.length; i++) {
                 labels.push(data[i].encounter_name);
-                ktimes.push(data[i].kd_sec);
+                ktTime.push(data[i].kd_sec);
             }
 
-            let trace = [{
-                x: labels,
-                y: ktimes,
-                name: "ZETA FC",
-                type: "bar"
-            }]
+            let ktData = [{
+                label: "kill time in seconds",
+                backgroundColor: "#03DAC5",
+                borderColor: "#03DAC5",
+                data: ktTime,
+            }];
 
-            Plotly.newPlot('gkt-overview-chart', trace);
+            let chartData = {
+                labels: labels,
+                datasets: ktData,
+            };
+
+            let config = {
+                type: 'bar',
+                data: chartData,
+                options: {
+                    responsive: true,
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: "Most recent fullclear",
+                        },
+                    },
+                    scales: {
+                        x: {
+                          grid: {
+                            display: false,
+                          }
+                        },
+                        y: {
+                          grid: {
+                            display: false,
+                          }
+                        }
+                    },
+                    layout: {
+                        autoPadding: true,
+                    },
+                }
+            };
+
+            let fcChart = new Chart(
+                document.getElementById('fc-chart'),
+                config
+            );
         });
     }
     function init() {
+        initColorLayout();
         initBarChart();
     }
     init();
